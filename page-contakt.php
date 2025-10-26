ef27a6982018096c9ef291b8e792f46e25e2a13b
1krylova-stack <anastasia1991krylova@gmail.com>
2025-10-26 12:55:51 +0300

    Update page-contakt.php

--- page-contakt.php
+++ page-contakt.php
@@ -1,40 +1,40 @@
 <?php
 /**
-  Template Name: Контакты
- *
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package WordPress
+ * Template Name: Контакты
  */
+get_header();
 ?>
 
-<?php get_header(); ?>
-	
-	<article class="full-width">
-		<div class="container">
-			<?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
+<style>
+  /* Скрываем СТАРЫЙ хедер только на этой странице */
+  .main_body > header { display:none !important; }
+</style>
 
-			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+<?php
+// Новый хедер (мега-панель "Страны", кнопка "Подобрать тур", иконки)
+get_template_part('header-new');
+?>
 
-				<div id="post-<?php the_ID(); ?>" class="page">
-					<?php if ( is_front_page() ) { ?>
-						<h2><?php the_title(); ?></h2>
-					<?php } else { ?>
-						<h1><?php the_title(); ?></h1>
-					<?php } ?>
-					<div class="entry-content">
+<article class="full-width">
+  <div class="container">
+    <?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
+    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+      <div id="post-<?php the_ID(); ?>" class="page">
+        <?php if ( is_front_page() ) { ?>
+          <h2><?php the_title(); ?></h2>
+        <?php } else { ?>
+          <h1><?php the_title(); ?></h1>
+        <?php } ?>
+        <div class="entry-content">
+          <?php if(has_post_thumbnail()) { ?>
+            <?php $full_img_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+            <a rel="lightbox[single]" href="<?php echo $full_img_url;?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('medium'); ?></a>
+          <?php } ?>
+          <?php the_content(); ?>
+        </div>
+      </div>
+    <?php endwhile; ?>
+  </div>
+</article>
 
-						<?php if(has_post_thumbnail()) { ?>
-							<?php $full_img_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
-							<a rel="lightbox[single]" href="<?php echo $full_img_url;?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('medium'); ?></a>
-						<?php } ?>
-						
-						<?php the_content(); ?>
-					</div>
-				</div>
-
-			<?php endwhile; ?>
-		</div>
-	</article>
 <?php get_footer(); ?>
